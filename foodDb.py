2339a70ab316907f93949633f14108f3e4a467aa
WhiteShadow13 <ludo.m@live.fr>
2018-11-06 17:40:39 +0100

    Fix add manga and tom

--- foodDb.py
+++ foodDb.py
@@ -19,57 +19,67 @@ class FoodDb():
         '''
 
         try:
-            session = self._driver.session()
-
             try:
+                session = self._driver.session()
                 request = 'CREATE (:Manga {name: {manga_name}})'
-                session.run(request, {'manga_name': manga_name})        
+                session.run(request, {'manga_name': manga_name})  
+                session.close()    
             except:
                 pass
 
             try:
+                session = self._driver.session()
                 request = 'CREATE (:Editor {name: {editor_name}})'
-                session.run(request, {'editor_name': editor_name})        
+                session.run(request, {'editor_name': editor_name})
+                session.close()    
             except:
                 pass
             
             try:
+                session = self._driver.session()
                 request = 'CREATE (:Type {name: {type_name}})'
-                session.run(request, {'type_name': type_name})        
+                session.run(request, {'type_name': type_name}) 
+                session.close()   
             except:
                pass
             
             try:
+                session = self._driver.session()
                 request = 'CREATE (:Author {firstname: {author_firstname}, lastname: {author_lastname}})'
-                session.run(request, {'author_firstname': author_firstname, 'author_lastname': author_lastname})        
+                session.run(request, {'author_firstname': author_firstname, 'author_lastname': author_lastname}) 
+                session.close()    
             except:
                 pass          
             
             try:
                 #Manga-[TYPE]->Type
+                session = self._driver.session()
                 request = "MATCH (m:Manga) WHERE m.name = '{}' MATCH (t:Type) WHERE t.name = '{}' CREATE (m) -[r:TYPE]-> (t)".format(manga_name, type_name)
                 session.run(request)
+                session.close()
             except:
                 pass        
             
             try:
                 #Author-[CREATE]->Manga
+                session = self._driver.session()
                 request = "MATCH (a:Author) WHERE a.firstname = '{}' AND a.lastname = '{}' MATCH (m:Manga) WHERE m.name = '{}' CREATE (a) -[r:CREATE]-> (m)".format(author_firstname, author_lastname, manga_name)
                 session.run(request)
+                session.close()
             except:
                 pass
 
             try:
                 #Editor-[EDITS]->Manga
+                session = self._driver.session()
                 request = "MATCH (e:Editor) WHERE e.name = '{}' MATCH (m:Manga) WHERE m.name = '{}' CREATE (e) -[r:EDITS]-> (m)".format(editor_name, manga_name)
                 session.run(request)
+                session.close()
             except:
                 pass
-
-            session.close()
         
         except:
-            session.close()
+            pass
 
 
     def addTom (self, manga_name, tom_title, tom_closingDate, tom_price, tom_possessed):
@@ -85,28 +95,30 @@ class FoodDb():
 
         try:
             session = self._driver.session()
-
             request = 'MATCH (m:Manga {name: {manga_name}}) RETURN m'
-            result = session.run(request, {'manga_name': manga_name})   
+            result = session.run(request, {'manga_name': manga_name}) 
+            session.close()  
 
             #Verify if the manga exists
             if (result.single() == None):
                 raise Exception('The manga does not exist') 
             
             try:
+                session = self._driver.session()
                 request = 'CREATE (:Tom {title: {tom_title}, closingDate: {tom_closingDate}, price: {tom_price}, possessed: {tom_possessed}})'
                 session.run(request, {'tom_title': tom_title, 'tom_closingDate': tom_closingDate, 'tom_price': tom_price, 'tom_possessed': tom_possessed})        
+                session.close()
             except:
                 pass
             
             try:
                 #Manga-[CONTAINS]->Tom
+                session = self._driver.session()
                 request = "MATCH (m:Manga) WHERE m.name = '{}' MATCH (t:Tom) WHERE t.title = '{}' CREATE (m) -[r:CONTAINS]-> (t)".format(manga_name, tom_title)
-                session.run(request)
+                session.run(request)              
+                session.close()
             except:
                 pass
-            
-            session.close()
 
         except Exception as e:
             print(e)
--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from foodDb import FoodDb
 
 food_db = FoodDb()
 
-#food_db.addManga('One-punch Man', 'Kurokawa', 'Seinen', 'Yusuke', 'Murata')
-food_db.addTom('One-punch Man', "One-punch Man T.1", '13/01/2016', 7.75, False)
-food_db.addTom('One-punch Man', "One-punch Man T.2", '16/03/2016', 7.75, False)
+food_db.addManga('Tokyo Ghoul', 'Glenat', 'Seinen', 'Sui', 'Ishida')
+food_db.addTom('Tokyo Ghoul', "Tokyo Ghoul T.1", '05/08/2015', 7.50, False)
+food_db.addTom('Tokyo Ghoul', "Tokyo Ghoul T.2", '05/08/2015', 7.50, False)
